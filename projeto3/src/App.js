6a62267141d8f775a74990724ee360786998cf76
Gino Oliveira <lucianaegino02@hotmail.com>
2022-04-26 14:47:37 -0300

    ultima aula

--- projeto3/src/App.js
+++ projeto3/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import React from 'react';
 import Home from './pages/Home'; 
 import Contato from './pages/Contato'; 
