9de57f1400de177225da76fb42f0c06648daf2b3
Gino Oliveira <lucianaegino02@hotmail.com>
2022-04-26 14:01:55 -0300

    React Router

--- projeto3/src/App.js
+++ projeto3/src/App.js
@@ -1,20 +1,28 @@
 import './App.css';
-import SeuNome from './components/SeuNome';
-import {useState} from 'react'
-import Saudacao from './components/Saudacao';
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import React from 'react';
+import Home from './pages/Home'; 
+import Contato from './pages/Contato'; 
+import Empresa from './pages/Empresa'; 
+import Navbar from './components/layout/Navbrar';
+import Footer from './components/layout/Footer';
+
+
 
 
 function App() {
-  
-  const [nome, setNome] = useState()
- 
+   
   return (
-    <div className="App">
-      <h1>State Lift</h1>
-      <SeuNome setNome={setNome}/>
-     <Saudacao nome={nome} />
-    </div>
-  );
+    <Router>
+     <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home />} ></Route>
+        <Route path="/contato" element={<Contato />} ></Route>
+        <Route path="/empresa" element={<Empresa />} ></Route>
+      </Routes>
+      <Footer/>
+    </Router>
+  )
 }
 
 export default App;
